83f23ca45bb0e4fed906196732950fd29748641b
Achraf Meadar <mqdashraf@gmail.com>
2026-09-01 10:53:01 +0200

    resolution de challenge 3 du variables

---
+++ day1/variables/challenge_3.js
@@ -0,0 +1,4 @@
+
+let stockage = 5;
+let Mb = `stockage en megabytes(Mb) : ${stockage * 1024}`, bytes = `stockage en bytes : ${stockage * (1024 ** 2)}`;    
+console.log("stockage en gigabytes(Gb) : " + stockage + "\n" + Mb + "\n" + bytes);
