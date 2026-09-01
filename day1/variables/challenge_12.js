0b96244b79e31487e2cc055fe3ac83567aa0c0ff
Achraf Meadar <mqdashraf@gmail.com>
2026-09-01 16:23:22 +0200

    correction de challenge 12

--- day1/variables/challenge_12.js
+++ day1/variables/challenge_12.js
@@ -2,7 +2,8 @@ const prompt = require('prompt-sync')();
 
 let code = prompt("Entrez un code de quatre chiffres : ");
 
-console.log("premier chiffre : " + code[0] +
-    "\ndeuxieme chiffre : " + code[1] +
-    "\ntroisieme chiffre : " + code[2] +
-    "\nquatrieme chiffre : " + code[3]);
+console.log("premier chiffre : " + code[4%4] +
+    "\ndeuxieme chiffre : " + code[4/4] +
+    "\ntroisieme chiffre : " + code[4/2] +
+    "\nquatrieme chiffre : " + code[12/4]);
+    
