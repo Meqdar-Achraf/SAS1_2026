c808c1d89202ba4572471b1bae7059a0c20ead06
Achraf Meadar <mqdashraf@gmail.com>
2026-09-01 11:05:00 +0200

    resolution de challenge 4 du variables

---
+++ day1/variables/challenge_4.js
@@ -0,0 +1,6 @@
+
+let distant = 1000, littre = 74 ;
+consommation = `consommation : ${(littre / distant) * 100} L/100Km` ;
+console.log ("distance parcourue : " + distant + " Km " + 
+    "\ncarburant consommé : " + littre + " L " + 
+    "\n" + consommation  ) ;
