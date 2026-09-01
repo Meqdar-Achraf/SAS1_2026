e88bbf4bcaa6f2f236662ccea7900a4083241548
Achraf Meadar <mqdashraf@gmail.com>
2026-09-01 10:22:28 +0200

    resultat de premier challenge du variables

---
+++ JS_test.js
@@ -0,0 +1,3 @@
+let fname = "Achraf", lname = "Meadar";
+
+console.log("xd" / 0);
---
+++ day1/variables/challenge_1.js
@@ -0,0 +1,5 @@
+
+let pseudo = "Kirito-47", niveau = 103, score = 1500, age = 21, pays = "maroc"
+
+console.log("Pseudo : " + pseudo + "\nNiveau : " + niveau +
+     "\nScore : " + score + "\nAge : " + age + "\nPays : " + pays);
