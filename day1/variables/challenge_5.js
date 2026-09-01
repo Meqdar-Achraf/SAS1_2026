2e2b4114929ca05c9f09405adca2851a4b2e52db
Achraf Meadar <mqdashraf@gmail.com>
2026-09-01 14:39:04 +0200

    correction de code de challenge 5 du variables

--- day1/variables/challenge_5.js
+++ day1/variables/challenge_5.js
@@ -4,12 +4,12 @@ let duree = prompt("vouillez enter la durée du film en minute : ");
 let categorie = ["Court métrage", "Film standard", "Film long"];
 
 if (duree <60 ) {
-    console.log("duree : " + duree + " minutes" + 
+    console.log("duree : " + duree + " minute" + 
         "\ncategorie : " + categorie[0]);
 } else if (duree > 60 && duree < 120) {
-    console.log("   duree : " + duree + " minutes" + 
+    console.log("   duree : " + duree + " minute" + 
         "\ncategorie : " + categorie[1]);
 } else {
-    console.log("duree : " + duree + " minutes" + 
+    console.log("duree : " + duree + " minute" + 
         "\ncategorie : " + categorie[2]);
 }
