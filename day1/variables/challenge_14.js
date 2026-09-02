880d1bfed42441783eda9dd1dddf8c873a5c2024
Achraf Meadar <mqdashraf@gmail.com>
2026-09-02 09:42:33 +0200

    Ajout du methode 2 (sans utilisation du troisieme variable )

--- day1/variables/challenge_14.js
+++ day1/variables/challenge_14.js
@@ -8,3 +8,16 @@ scoreP1 = score3
 
 console.log("joueur A : " + scoreP1 +
     "\njoueur B : " + scoreP2);
+
+// Methode 2 : sans troisieme variable !!!
+
+
+let score1 = prompt("Entrez le score du joueur A : ");
+let score2 = prompt("Entrez le score du joueur B : ");
+
+score1 = score1 - score2;
+score2 = score1 + score2;
+score1 = score2 - score1;
+
+console.log("joueur A : " + score1 +
+    "\njoueur B : " + score2);
