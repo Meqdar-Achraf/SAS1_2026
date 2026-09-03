48f96fbe222f22f7434d0cb5bedb7ed416094f50
Achraf Meadar <mqdashraf@gmail.com>
2026-09-03 13:51:29 +0200

    resolution de challenge 24 du boucles

--- day2/boucles1/challenge24.js
+++ day2/boucles1/challenge24.js
@@ -2,6 +2,7 @@ const prompt = require('prompt-sync')();
 
 let number = prompt("entrez un nomber :");
 let triangle = 0;
+
 for (i=1 ; i<= number ; i++ ){
     triangle = triangle * 10 + i
     console.log(triangle)
