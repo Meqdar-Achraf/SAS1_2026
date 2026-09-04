86d57ba9b8b52749fae354d7adb533f37b8cfbbe
Achraf Meadar <mqdashraf@gmail.com>
2026-09-04 10:13:18 +0200

    resolution de challenge 2 du chaines de caracteres

--- day4/chaines-de-caracteres/challenge2.js
+++ day4/chaines-de-caracteres/challenge2.js
@@ -1,4 +1,14 @@
+const prompt = require('prompt-sync')();
 
 function compterLettre(chaine, lettre){
+    let occurence = 0;
     
+    for (str of chaine){
+        if (str === lettre) occurence ++;
+    }
+    return occurence ;
 }
+let word = prompt("entrez un mots : ");
+let lett = prompt("entrez un lettre : ")
+
+console.log(`le nombre d'occurence de "${lett}" dans ${word} : ${compterLettre(word, lett)}`)
