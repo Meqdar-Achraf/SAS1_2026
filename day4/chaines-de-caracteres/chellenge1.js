eb13b63d127e0cd174e5827076ae0f2a19333dea
Achraf Meadar <mqdashraf@gmail.com>
2026-09-04 10:00:23 +0200

    resolution de challenge 1 du chaine de caracteres

---
+++ day4/chaines-de-caracteres/chellenge1.js
@@ -0,0 +1,13 @@
+
+const prompt = require('prompt-sync')();
+
+function compterCaracteres(chaine) {
+    let i = 0
+    
+    for (let str of chaine){
+        i++
+    }
+    console.log(`nombre de caracteres : ${i}`)
+}
+
+compterCaracteres ("hello guys")
